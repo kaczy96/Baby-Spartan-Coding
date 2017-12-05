2f2850384cb867459fc6ebaaf71d3375140fe298
kaczy96 <kaczystudio@gmail.com>
2017-12-05 13:20:52 +0100

    update

---
+++ Assets/Controller.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+    public float maxSpeed = 10f;
+    bool facingRight = true;
+	void Start () {
+		
+	}
+	
+	void Update () {
+        var move = Input.GetAxis("Horizontal");
+        rigidbody2D.velocity = new Vector2(move * maxSpeed, rigidbody2D.velocity.y);
+	}
+}
