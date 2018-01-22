0075f4ea8bef0b9fb145f96250262a732aa430c2
Tomasz Niedźwiedź <kaczystudio@gmail.com>
2018-01-22 23:39:44 +0100

    New scripts

---
+++ Assets/ThroughThePortal.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThroughThePortal : MonoBehaviour {
+
+    public GameObject otherPortal;
+
+	void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("I toch a portal");
+        if(other.tag == "Player")
+        {
+            other.transform.position = otherPortal.transform.position + otherPortal.transform.forward * 1;
+        }
+    }
+
+}
---
+++ Assets/ThrowPortal.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowPortal : MonoBehaviour {
+
+    public GameObject leftPortal;
+    public GameObject rightPortal;
+
+	void Start () {
+		
+	}
+	
+	void Update ()
+    {
+		if(Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Left click");
+            throwPortal(leftPortal);
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log("Right click");
+            throwPortal(rightPortal);
+        }
+
+    }
+
+    void throwPortal(GameObject portal)
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            Quaternion hitObjectRotation = Quaternion.LookRotation(hit.normal);
+            portal.transform.position = hit.point;
+            portal.transform.rotation = hitObjectRotation;
+        }
+            
+
+    }
+
+}
