b890fa9e40b62d78ef312d1f17ffe201490ce2d4
Tomasz Niedźwiedź <kaczystudio@gmail.com>
2018-01-25 19:55:04 +0100

    Update

---
+++ Assets/Scripts/EnemyPatrol.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour {
+
+    public float moveSpeed;
+    public bool moveRight;
+    public Transform wallCheck;
+    public float wallCheckRadius;
+    public LayerMask whatIsWall;
+    private bool wall;
+
+    void Start ()
+    {
+		
+	}
+	
+	void Update ()
+    {
+        wall = Physics2D.OverlapCircle(wallCheck.position, wallCheckRadius, whatIsWall);
+
+        if(wall)
+        {
+            moveRight = !moveRight;
+        }
+
+        if (moveRight)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+        else
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        }
+	}
+}
