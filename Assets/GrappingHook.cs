99fb15a76b61f58e57a04f8300946b3de75246b6
Tomasz Niedźwiedź <kaczystudio@gmail.com>
2018-01-25 21:22:47 +0100

    Hook pre-alpha

---
+++ Assets/GrappingHook.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrappingHook : MonoBehaviour {
+
+    DistanceJoint2D joint;
+    Vector3 targetPos;
+    RaycastHit2D hit;
+    public float distance = 10f;
+    public LayerMask hookable;
+
+	void Start ()
+    {
+       joint = GetComponent<DistanceJoint2D>();
+        joint.enabled = false;
+	}
+	
+	void Update ()
+    {
+		if(Input.GetKeyDown(KeyCode.E))
+        {
+            targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            targetPos.z = 0;
+            hit = Physics2D.Raycast(transform.position, targetPos - transform.position, distance, hookable);
+
+            if (hit.collider != null && hit.collider.gameObject.GetComponent<Rigidbody2D>() != null)
+                
+            {
+                joint.enabled = true;
+                joint.connectedBody = hit.collider.gameObject.GetComponent<Rigidbody2D>();
+                joint.distance = Vector2.Distance(transform.position, hit.point);
+            }
+
+            
+
+        }
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            joint.enabled = false;
+        }
+    }
+}
---
+++ Assets/Opacity.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Opacity : MonoBehaviour {
+
+    public SpriteRenderer spawnPoint;
+
+	void Start ()
+    {
+
+        
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        spawnPoint.color = new Color(1f, 1f, 1f, .5f);
+    }
+}
