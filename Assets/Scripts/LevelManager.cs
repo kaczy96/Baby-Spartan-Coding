cd5cba47483aa1eb81007d5341424e347b332e3d
Tomasz Niedźwiedź <kaczystudio@gmail.com>
2018-10-27 13:14:55 +0200

    Deleted old respawn mechanic and made it simpler

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -23,24 +23,8 @@ public class LevelManager : MonoBehaviour {
 
     public void RespawnPlayer()
     {
-        StartCoroutine("RespawnPlayerCo");
+        player.transform.position = currentCheckpoint.transform.position;
     }
 
-    public IEnumerator RespawnPlayerCo()
-    {
-        Instantiate(deathParticle, player.transform.position, player.transform.rotation);
-        player.enabled = false;
-        player.GetComponent<Renderer>().enabled = false;
-        gravityStore = player.GetComponent<Rigidbody2D>().gravityScale;
-        player.GetComponent<Rigidbody2D>().gravityScale = 0f;
-        player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
-        Debug.Log("You lost da wae, I will show u da wae");
-        yield return new WaitForSeconds(respawnDelay);
-        player.GetComponent<Rigidbody2D>().gravityScale = gravityStore;
-        player.transform.position = currentCheckpoint.transform.position;
-        player.enabled = true;
-        player.GetComponent<Renderer>().enabled = true;
-        Instantiate(respawnParticle, currentCheckpoint.transform.position, currentCheckpoint.transform.rotation);
-    }
 
 }
