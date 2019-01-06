2d49e54f4312af9269c39e81c742c22c033f52b9
Tomasz Niedźwiedź <kaczystudio@gmail.com>
2019-01-06 21:11:19 +0100

    Sprites and basic map
    Texture
    Sprites
    and basic level

---
+++ Assets/DeathZone.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeathZone : MonoBehaviour
+{
+    private Player player;
+
+    public Image healthBar;
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            
+            healthBar.fillAmount = 0;
+            player.health = 0;
+
+        }
+    }
+}
+
+
