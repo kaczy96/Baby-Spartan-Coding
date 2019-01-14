c92ae807727ba184086e21895c82bcf7fde4b600
eventowy <damianxwojcik@gmail.com>
2019-01-14 20:34:26 +0100

    Additional stuff; Finished Hades scene for trailer
    Camera movement script for trailer; Finished Hades scene

---
+++ Assets/Scripts/CameraMoves.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMoves : MonoBehaviour
+{
+    public float speed;
+    public Camera Camera;
+
+    public void Start()
+    {
+
+    }
+    public void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.Keypad6))
+        {
+            transform.position = new Vector3(transform.position.x + speed, transform.position.y, transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.Keypad4))
+        {
+            transform.position = new Vector3(transform.position.x - speed, transform.position.y, transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.Keypad2))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - speed, transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.Keypad8))
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + speed, transform.position.z);
+        }
+        if (Input.GetKey(KeyCode.N))
+        {
+            Camera.orthographicSize++;
+        }
+    }
+}
---
+++ Assets/Scripts/LavaBall.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaBall : MonoBehaviour
+{
+    public GameObject objectToMove;
+    public Transform StartPoint;
+    public Transform EndPoint;
+    private SpriteRenderer mySpriteRenderer;
+    public float movingSpeed;
+    private Vector3 actualTarget;
+
+    public Animator anim;
+
+    void Start()
+    {
+        actualTarget = EndPoint.position;
+        anim = GetComponent<Animator>();
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        objectToMove.transform.position = Vector3.MoveTowards(objectToMove.transform.position, actualTarget, movingSpeed * Time.deltaTime);
+        if (objectToMove.transform.position == EndPoint.position)
+        {
+            actualTarget = StartPoint.position;
+        }
+        else if (objectToMove.transform.position == StartPoint.position)
+        {
+            actualTarget = EndPoint.position;
+        }
+    }
+
+}
